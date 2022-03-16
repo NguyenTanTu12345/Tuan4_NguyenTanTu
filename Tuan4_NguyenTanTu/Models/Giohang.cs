4cab0d275a865c8cf4e3727b9670a0231ba6aa45
NguyenTanTu <NguyenTanTu@LAPTOP-GQG4KP7A>
2022-03-16 10:55:54 +0700

    Add project files.

---
+++ Tuan4_NguyenTanTu/Models/Giohang.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using Tuan4_NguyenTanTu.Models;
+
+namespace Tuan4_NguyenTanTu.Models
+{
+    public class Giohang
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        [DisplayName("Mã sách : ")]
+        public int masach { get; set; }
+        [DisplayName("Tên sách : ")]
+        public string tensach { get; set; }
+        [DisplayName("Ảnh bìa : ")]
+        public string hinh { get; set; }
+        [DisplayName("Giá bán : ")]
+        public Double giaban { get; set; }
+        [DisplayName("Số lượng : ")]
+        public int iSoluong { get; set; }
+        [DisplayName("Thành tiền : ")]
+        public Double dThanhtien { 
+            get { return iSoluong * giaban; }
+        }
+        public Giohang(int id)
+        {
+            masach = id;
+            Sach sach = data.Saches.Single(n => n.masach == masach);
+            tensach = sach.tensach;
+            hinh = sach.hinh;
+            giaban = double.Parse(sach.giaban.ToString());
+            iSoluong = 1;
+        }
+    }
+}
